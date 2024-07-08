8d3db11430d350856701f5cab6fc185c05230e26
João Paulo <jppokedev@gmail.com>
2024-07-08 05:18:09 -0300

    Add movimento das peças

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -8,7 +8,7 @@ public class Board {
 	
 	public Board(Integer rows, Integer columns) {
 		if(rows < 1 || columns < 1) {
-			throw new BoardExceptio("Error creating board: there must be at least 1 row and 1 column");
+			throw new BoardException("Error creating board: there must be at least 1 row and 1 column");
 		}
 		this.rows = rows;
 		this.columns = columns;
@@ -23,24 +23,37 @@ public class Board {
 
 	public Piece piece(Integer row, Integer column) {
 		if(!positionExists(row, column)) {
-			throw new BoardExceptio("Position not on the board");
+			throw new BoardException("Position not on the board");
 		}
 		return pieces[row][column];
 	}
 	
 	public Piece piece(Position position) {
 		if(!positionExists(position)) {
-			throw new BoardExceptio("Position not on the board");
+			throw new BoardException("Position not on the board");
 		}
 		return pieces[position.getRow()][position.getColumn()];
 	}
 	
 	public void placePiece(Piece piece, Position position) {
 		if (thereIsAPiece(position)) {
-			throw new BoardExceptio("There is already a piece on position "+ position);
+			throw new BoardException("There is already a piece on position "+ position);
 		}
 		pieces[position.getRow()][position.getColumn()] = piece;
 		piece.position = position;
+	}
+	
+	public Piece removePiece(Position position) {
+		if (!positionExists(position)) {
+			throw new BoardException("Position not on the board");
+		}
+		if(piece(position) == null) {
+			return null;
+		}
+		Piece aux = piece(position);
+		aux.position = null;
+		pieces[position.getRow()][position.getColumn()] = null;
+		return aux;
 	}
 	
 	private Boolean positionExists(Integer row, Integer column) {
@@ -53,7 +66,7 @@ public class Board {
 	
 	public Boolean thereIsAPiece(Position position) {
 		if(!positionExists(position)) {
-			throw new BoardExceptio("Position not on the board");
+			throw new BoardException("Position not on the board");
 		}
 		return piece(position) != null;
 	}
--- src/boardgame/BoardExceptio.java
+++
@@ -1,9 +0,0 @@
-package boardgame;
-
-public class BoardExceptio extends RuntimeException {
-	private static final long serialVersionUID = 1L;
-
-	public BoardExceptio(String msg) {
-		super(msg);
-	}
-}
